8651d2e144686ed047709733376bcfb460ecb94d
Boris Kaus <kaus@uni-mainz.de>
2025-12-18 16:53:27 +0100

    read slices in xy, xz and yz direction

--- src/read_output.jl
+++ src/read_output.jl
@@ -2,15 +2,23 @@
 # This assumes data to be saved in netcdf format
 using NCDatasets, GeophysicalModelGenerator
 
-export read_xy_slice
+export read_xy_slice, read_yz_slice, read_xz_slice
 
 slice_xy(array::Array{T,2}) where T = reshape(array[:,:], size(array[:,:])...,1)
+slice_xz(array::Array{T,2}) where T = reshape(array[:,:], size(array,1),1,size(array,2))
+slice_yz(array::Array{T,2}) where T = reshape(array[:,:], 1,size(array,1),size(array,2))
 
 isnetcdf(file::AbstractString) = endswith(lowercase(file), ".nc")
 create_tuple_field(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,1}) where T = NamedTuple{(Symbol(name),)}((field[:],))
-create_tuple_field_slice(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,2}) where T = NamedTuple{(Symbol(name),)}((slice_xy(field[:,:]),))
-create_tuple_field_slice(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,3}, itime) where T = NamedTuple{(Symbol(name),)}((slice_xy(field[:,:,itime]),))
 create_tuple_field(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,3}) where T = NamedTuple{(Symbol(name),)}((field[:,:,:],))
+
+create_tuple_field_slice_xy(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,2}) where T = NamedTuple{(Symbol(name),)}((slice_xy(field[:,:]),))
+create_tuple_field_slice_xy(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,3}, itime) where T = NamedTuple{(Symbol(name),)}((slice_xy(field[:,:,itime]),))
+create_tuple_field_slice_xz(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,2}) where T = NamedTuple{(Symbol(name),)}((slice_xz(field[:,:]),))
+create_tuple_field_slice_xz(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,3}, itime) where T = NamedTuple{(Symbol(name),)}((slice_xz(field[:,:,itime]),))
+create_tuple_field_slice_yz(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,2}) where T = NamedTuple{(Symbol(name),)}((slice_yz(field[:,:]),))
+create_tuple_field_slice_yz(name::String, field::NCDatasets.CommonDataModel.CFVariable{T,3}, itime) where T = NamedTuple{(Symbol(name),)}((slice_yz(field[:,:,itime]),))
+
 
 """
     data, t  = read_xy_slice(file::AbstractString, depth=0.0; timestep::Int=1, cart_data=false) 
@@ -39,7 +47,7 @@ julia> file = joinpath(dir, files[1])
 julia> data = read_xy_slice(file, -2.5, timestep=5, cart_data=true)
 ```
 """
-function read_xy_slice(file::AbstractString, depth=0.0; timestep::Int=1, cart_data=false)
+function read_xy_slice(file::AbstractString, depth=0.0; timestep::Int=1, cart_data=true)
     ds = NCDataset(file)
     @assert isnetcdf(file) "File $file is not a NetCDF file"
     @assert haskey(ds, "y") "File $file does not contain variable 'y'"
@@ -51,18 +59,7 @@ function read_xy_slice(file::AbstractString, depth=0.0; timestep::Int=1, cart_da
     lon = ds["lon"][:,:]
     lat = ds["lat"][:,:]
 
-    fields = (;)
-    for varname in keys(ds)
-        # add all 2D fields
-        if !(varname in ("x", "y", "lon", "lat"))
-            if length(size(ds[varname])) == 2
-                fields = merge(fields, create_tuple_field_slice(varname, ds[varname]))
-            elseif length(size(ds[varname])) == 3
-                fields = merge(fields, create_tuple_field_slice(varname, ds[varname], timestep))
-            end
-        end
-    end
-
+    fields = extract_fields_slice(ds, timestep, ("x","y"))
 
     Lon = zeros(size(lon,1), size(lon,2),1)
     Lat = zeros(size(lon,1), size(lon,2),1)
@@ -76,4 +73,72 @@ function read_xy_slice(file::AbstractString, depth=0.0; timestep::Int=1, cart_da
     end
 
     return out, t[timestep]
+end
+
+"""
+    data, t  = read_xz_slice(file::AbstractString, y0=0.0; timestep::Int=1)
+extracts a x,z slides at given y0 from netcdf file
+"""
+function read_xz_slice(file::AbstractString, y0=0.0; timestep::Int=1)
+    ds = NCDataset(file)
+    @assert isnetcdf(file) "File $file is not a NetCDF file"
+    @assert haskey(ds, "z") "File $file does not contain variable 'z'"
+
+    t  = ds["t"][:]
+    fields = extract_fields_slice(ds, timestep, ("x","z"))
+    
+    X,Y,Z = xyz_grid(ds["x"][:],y0,ds["z"][:]);
+    out = CartData(X,Y,Z, fields)  
+   
+    return out, t[timestep]
+end
+
+"""
+    data, t  = read_yz_slice(file::AbstractString, x0=0.0; timestep::Int=1)
+extracts a y,z slides at given x0 from netcdf file
+"""
+function read_yz_slice(file::AbstractString, x0=0.0; timestep::Int=1)
+    ds = NCDataset(file)
+    @assert isnetcdf(file) "File $file is not a NetCDF file"
+    @assert haskey(ds, "z") "File $file does not contain variable 'z'"
+    @assert haskey(ds, "y") "File $file does not contain variable 'y'"
+
+    t  = ds["t"][:]
+    fields = extract_fields_slice(ds, timestep, ("y","z"))
+    
+    X,Y,Z = xyz_grid(x0, ds["y"][:], ds["z"][:]);
+    out = CartData(X,Y,Z, fields)  
+   
+    return out, t[timestep]
+end
+
+# extract slices and converts them in a format that GMG needs
+function extract_fields_slice(ds::NCDataset, timestep::Int, types=("x","y"))
+    fields = (;)
+    for varname in keys(ds)
+        # add all 2D fields
+        if !(varname in types)
+            if length(size(ds[varname])) == 2
+                if types == ("x","y")
+                    fields = merge(fields, create_tuple_field_slice_xy(varname, ds[varname]))
+                elseif types == ("x","z")
+                    fields = merge(fields, create_tuple_field_slice_xz(varname, ds[varname]))
+                elseif types == ("y","z")
+                    fields = merge(fields, create_tuple_field_slice_yz(varname, ds[varname]))
+                else
+                    error("Unsupported variable dimension for types $types")
+                end
+            elseif length(size(ds[varname])) == 3
+                if types == ("x","y")
+                    fields = merge(fields, create_tuple_field_slice_xy(varname, ds[varname], timestep))
+                elseif types == ("x","z")
+                    fields = merge(fields, create_tuple_field_slice_xz(varname, ds[varname], timestep))
+                elseif types == ("y","z")
+                    fields = merge(fields, create_tuple_field_slice_yz(varname, ds[varname], timestep))
+                end    
+            end
+        end
+    end
+
+    return fields
 end

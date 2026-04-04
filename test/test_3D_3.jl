b8c67ac2d0fa9da0400c139b12d03cc97637e85b
Boris Kaus <kaus@uni-mainz.de>
2026-04-04 19:01:03 +0200

    add 3D test with free surface and seismometers

--- test/test_3D_3.jl
+++ test/test_3D_3.jl
@@ -16,9 +16,6 @@
 # load this with:
 Topo = load_GMG("topo_LaPalma")
 
-
-
-
 # project to cartesian coordinates
 proj = ProjectionPoint(Lon=-17.84, Lat=28.56)
 Topo_model = CartData(xyz_grid(-35:.1:30,-15:.2:45,0));
@@ -26,7 +23,9 @@ Topo_model = project_CartData(Topo_model, Topo, proj)
 write_paraview(Topo_model, "topo_LaPalma.vts")
 
 # Define a 3D velocity model
-N = 128,128,128
+#N = 128,128,128
+N = 256,256,256
+
 x = range(-20,20,length=N[1])
 y = range(-15,35,length=N[2])
 z = range(-20,5,length=N[3])
@@ -59,16 +58,18 @@ lambda = rho.* (vp .^2 .- 2 .* vs .^2)
 
 # Add fields to the CartData structure
 vmod = addfield(vmod,(;rho,Qp,Qs,mu,lambda, vp, vs))
-write_paraview(vmod, "vmod_LaPalma")        # save to disk
+#write_paraview(vmod, "vmod_LaPalma")        # save to disk
 
 
 
-s1 = SourceLLMWDC( -17.84, 28.56,  10.0,  0.1,    4,   3.9, 243.3627, 31.597,  -73.886 )
+#s1 = SourceLLMWDC( -17.84, 28.56,  10.0,  0.1,    2,   2.9, 243.3627, 31.597,  -73.886 )
+s1 = SourceXYMWDC( 0, 0.1,  10.0,  0.1,    2,   2.9, 243.3627, 31.597,  -73.886 )
 
+#"dep" sets the station at the 3D location
 stations = [
-    StationXY( 0.8,  -0.3, 0.170, "CAAM", "dep"),
-    StationXY( 1.5,   0.5, 0.110, "CSTH", "oba"),
-    ]
+    StationXY( 0.8,  -0.3,  0.170, "STA1", "dep"),
+    StationXY( 1.5,   14.0, 1.210, "STA2", "dep"),
+]
 
 # Specify model
 cfg = OpenSWPCConfig(vmod,
@@ -76,20 +77,20 @@ cfg = OpenSWPCConfig(vmod,
                         odir="cf_swp_lapalma",
                         tbeg=0.0,
                         nproc_x=2, nproc_y=2,
-                        dt = 0.02, vcut=0.1, nt=600,
+                        dt = 0.01, vcut=0.1, nt=1000,
                         clon = proj.Lon, clat = proj.Lat,
                         xy_ps_sw=true,xz_ps_sw=true,yz_ps_sw=true,
-                        vol_v_sw=true, vol_u_sw=false, vol_ps_sw=false,
+                        vol_v_sw=false, vol_u_sw=false, vol_ps_sw=false,
                         z0_xy=2.5,stftype="triangle", 
                         
                         # set the horizontal reference depth above the free surface (topography); the actual air parameters are already set in 3d
                         topo0 = -maximum(Topo_model.z.val), 
 
-                        # save every grid point, and every 10 timesteps:
-                        ntdec_s=10,idec=1, jdec=1, kdec=1,  
+                        # save every grid point, and every 20 timesteps:
+                        ntdec_s=20,idec=1, jdec=1, kdec=1,  
 
                         # waveform output
-                        sw_wav_u=true,sw_wav_v=false, ntdec_w=10,
+                        sw_wav_u=true,sw_wav_v=true, ntdec_w=10,
 
                         source=[s1], stations=stations)
 
@@ -99,14 +100,17 @@ run_swpc(cfg)
 
 # Read some output back
 ntsteps_saved = cfg.nt ÷ cfg.ntdec_s
-dat,t=read_volume(joinpath(cfg.odir, "swpc.3d.vol.v.nc"), timestep=ntsteps_saved)
+
+dat,t=read_xz_slice(joinpath(cfg.odir, "swpc.3d.xz.v.nc"), timestep=ntsteps_saved)
 V = dat.fields.var"V_m/s"
+@test  sum(V[1]) ≈ -0.00026084436f0 rtol=1e-4
 
 # Convert all NetCDF output files to ParaView format (*.pvd + *.vts)
 movie_slice(cfg)
 
+#=
 clean(cfg)
 rm("topo_LaPalma.vts")  
 rm("vmod_LaPalma.vts")  
-
+=#
 
